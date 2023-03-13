9ac12aa8433c9a0d3fab808b78358f647232b7d6
phucchuotcr7 <tranminhphuc26082002@gmail.com>
2023-03-13 21:01:40 +0700

    master

---
+++ slider.js
@@ -0,0 +1,32 @@
+var detail_img_laptop = [
+    'images/laptop1.jpg',
+    'images/images-detail1/detail1-laptop1.png',
+    'images/images-detail1/detail2-laptop1.png',
+    'images/images-detail1/detail3-laptop1.jpg',
+    'images/images-detail1/detail4-laptop1.webp',
+]
+
+function first() {
+    var slider = document.getElementById('img-slider');
+    slider.src = detail_img_laptop[0]
+}
+
+function second() {
+    var slider = document.getElementById('img-slider');
+    slider.src = detail_img_laptop[1];
+}
+
+function third() {
+    var slider = document.getElementById('img-slider');
+    slider.src = detail_img_laptop[2];
+}
+
+function four() {
+    var slider = document.getElementById('img-slider');
+    slider.src = detail_img_laptop[3];
+}
+
+function five() {
+    var slider = document.getElementById('img-slider');
+    slider.src = detail_img_laptop[4];
+}
